eadf813adc23b544b325500b6edb765a86f2853d
Miguel González <minimegapro@gmail.com>
2025-11-17 16:17:44 +0100

    Added PanelBusquedas.php

---
+++ Practicas Evaluables/Practica 2/PanelBusquedas.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Panel Búsquedas</title>
+</head>
+<body>
+    Este es el panel de búsquedas (SELECT)
+    <button>Cerrar sesion</button>
+</body>
+</html>
